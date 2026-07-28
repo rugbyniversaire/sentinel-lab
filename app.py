16397e2319929f889d8bfb50d7580b1824ffb280
rugbyniversaire <rugbyniversaire@gmail.com>
2026-07-28 11:13:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -88,21 +88,7 @@ from core.filters import (
 
 
 
-def rechercher_tout(mot_cle):
-    maintenant = datetime.now(timezone.utc)
-    seuil = maintenant - timedelta(hours=LIMITE_HEURES)
-    tous = (
-        chercher_google_news(mot_cle, seuil)
-        + chercher_reddit(mot_cle, seuil)
-        + chercher_hackernews(mot_cle, seuil)
-        + chercher_mastodon(mot_cle, seuil)
-        + chercher_bluesky(mot_cle, seuil)
-        + chercher_youtube(mot_cle, seuil)
-    )
-    tous = [r for r in tous if est_pertinent(mot_cle, r["titre"])]
-    tous = dedupliquer(tous)
-    tous.sort(key=lambda r: r["date_pub"], reverse=True)
-    return tous, maintenant
+
 
 
 def afficher_resultats(resultats, maintenant, ne_garder_que_regroupes=False):
