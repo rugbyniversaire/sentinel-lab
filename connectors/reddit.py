420bd67679b79cbb3e06f1bc0b82fbd89b9d918a
rugbyniversaire <rugbyniversaire@gmail.com>
2026-07-28 16:35:54 +0200

    Update reddit.py

--- connectors/reddit.py
+++ connectors/reddit.py
@@ -1,16 +1,39 @@
+import feedparser
+
+from datetime import datetime, timezone
+from urllib.parse import quote
+
+from core.filters import est_recent
+from core.utils import extraire_image_html
+
+
 def chercher_reddit(mot_cle, seuil):
     url = f"https://old.reddit.com/search.rss?q={quote(mot_cle)}&sort=new"
+
     try:
         flux = feedparser.parse(url)
+
         resultats = []
+
         for entree in flux.entries:
+
             date_pub = None
+
             if entree.get("published_parsed"):
                 date_pub = datetime(*entree.published_parsed[:6], tzinfo=timezone.utc)
+
             if est_recent(date_pub, seuil):
                 image = extraire_image_html(entree.get("summary", ""))
-                resultats.append({"source": "Reddit", "titre": entree.title, "lien": entree.link, "date_pub": date_pub, "image": image})
+
+                resultats.append({
+                    "source": "Reddit",
+                    "titre": entree.title,
+                    "lien": entree.link,
+                    "date_pub": date_pub,
+                    "image": image
+                })
+
         return resultats
+
     except Exception:
         return []
-
