3b5a11e0eeab426b56d5c48b304d61a15b7ca3a9
rugbyniversaire <rugbyniversaire@gmail.com>
2026-07-28 11:01:07 +0200

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,1 +1,14 @@
+def parser_date_iso(chaine):
+    """Parse une date ISO 8601 de façon tolérante (gère les fractions de secondes non standard)."""
+    if not chaine:
+        return None
+    chaine = chaine.replace("Z", "+00:00")
+    match = re.match(r"^(.*?\.\d{1,6})\d*(\+.*)$", chaine)
+    if match:
+        chaine = match.group(1) + match.group(2)
+    try:
+        return datetime.fromisoformat(chaine)
+    except ValueError:
+        return None
+
 
