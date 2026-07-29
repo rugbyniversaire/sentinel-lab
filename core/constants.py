fd716f817a30c5a4ee0e2bac5a0a52da6a4491c1
rugbyniversaire <rugbyniversaire@gmail.com>
2026-07-29 16:29:29 +0200

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -1,10 +1,13 @@
 LIMITE_HEURES = 24
+
 SEUIL_SIMILARITE = 0.42
 
 MOTS_VIDES = {
-    "le", "la", "les", "un", "une", "des", "de", "du", "et", "en", "au", "aux",
-    "pour", "avec", "sur", "dans", "par", "est", "sont", "qui", "que", "son",
-    "sa", "ses", "ce", "cette", "ces", "il", "elle", "ils", "elles", "vers",
-    "ne", "pas", "plus", "leur", "leurs", "après", "avant", "the", "and", "of",
-    "to", "in", "a", "an", "is", "for", "on", "with", "at", "by"
+    "le", "la", "les", "un", "une", "des", "de", "du", "et", "en",
+    "au", "aux", "pour", "avec", "sur", "dans", "par", "est", "sont",
+    "qui", "que", "son", "sa", "ses", "ce", "cette", "ces", "il",
+    "elle", "ils", "elles", "vers", "ne", "pas", "plus", "leur",
+    "leurs", "après", "avant",
+    "the", "and", "of", "to", "in", "a", "an", "is", "for", "on",
+    "with", "at", "by"
 }
